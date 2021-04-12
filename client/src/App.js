42e28fdb82eae1871fa9dd85fc7d1b7bedf7fb95
Drew A <alvarez.drew@gmail.com>
2021-04-12 17:05:25 -0500

    react start/removed default stuff

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="App-intro">
+        This is a Placeholder for Texas Tom's
+      </p>
+    </div>
+  );
+}
+
+
+export default App;
